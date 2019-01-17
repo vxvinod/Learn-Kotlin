61a2dbcb3aaffef97260835eaf114553110551b2
vxvinod <vinodkmrmurugan@gmail.com>
2019-01-17 14:54:38 +0530

    Day 1 hello world

---
+++ 1_hello_world.kt
@@ -0,0 +1,9 @@
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    println("Hello, World.")
+    println("Welcome to 30 Days of Code!")
+}
+
